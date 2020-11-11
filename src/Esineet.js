33dbddf33ca1ec42c51dd69867b22667aa94eacb
laineantti <laineantti@users.noreply.github.com>
2020-11-11 15:51:16 +0200

    Päivitetty opettajan versioon

--- src/Esineet.js
+++ src/Esineet.js
@@ -9,30 +9,28 @@ const Esineet = (props) => {
     const [dataNoudettu, setDataNoudettu] = useState(false)
     const [valittuPaikkakunta, setValittuPaikkakunta] = useState("")
     const [hakusana, setHakusana] = useState("")
+
     useEffect(async () => {
-        let result = await axios('https://api.huuto.net/1.1/categories/' + props.id + '/items?area=' + valittuPaikkakunta + '&words=' + hakusana
-        )
-        //console.log(result)
-        //let a = JSON.parse(result.request.response).items
+        let result = await axios('https://api.huuto.net/1.1/categories/' + props.id + '/items?area=' + valittuPaikkakunta + '&words=' + hakusana)
         setData(JSON.parse(result.request.response).items);
         setDataNoudettu(true);
 
     }, [props.id, valittuPaikkakunta, hakusana])
 
-    const paikkakuntaTamperePainettu = () => {
-        setValittuPaikkakunta("Tampere")
-    }
-    const paikkakuntaHelsinkiPainettu = () => {
-        setValittuPaikkakunta("Helsinki")
+    const paikkakuntaMuutetaan = (event) => {
+        setValittuPaikkakunta(event.target.value)
     }
     const hakusanaMuutetaan = (event) => {
         setHakusana(event.target.value)
     }
 
     return (<div>
-        <button onClick={paikkakuntaTamperePainettu}>Tampere</button>
-        <button onClick={paikkakuntaHelsinkiPainettu}>Helsinki</button>
-        <input onChange={(event)=>hakusanaMuutetaan(event)}></input>
+        <hr />
+        <pre>
+            Hakusana:                   <input onChange={(event) => hakusanaMuutetaan(event)}></input><br />
+            Paikkakunta/postinumero:    <input onChange={(event) => paikkakuntaMuutetaan(event)}></input>
+        </pre>
+        <hr />
 
         {!dataNoudettu ? "Odotetaan dataa..." : data.map(alkio => <div>{alkio.title + " Paikassa:" + alkio.location}</div>)}
 
