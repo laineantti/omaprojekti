1bedf8285f27fcfb9e2eefabf58fce7b9d002c57
laineantti <laineantti@users.noreply.github.com>
2020-11-11 16:11:28 +0200

    Päivitetty opettajalta joitain uusia osia koodista

--- src/App.js
+++ src/App.js
@@ -1,44 +1,136 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import Esineet from './Esineet'
-//useReducer = REDUX
-//Context API
+import React, { useEffect } from 'react';
+import { useState } from 'react'
+import LapsiLista from './LapsiLista'
+import uuid from 'react-uuid'
+//import './App.css';
+// lukumäärä???
 
-//Axios, useEffect
-const App = () => {
 
-  //    const [data,setData] = useState(["Dataa haetaan"])
+//let u = {0:"pekka",1:"leena"}
 
+function App() {
+  //array destructuring 
+  //let lapset = [{lapsenNimi:"Lissa"},{lapsenNimi:"Kaapo"}] 
   const [data, setData] = useState([])
-  const [kategoriaValittu, setKategoriaValittu] = useState(false)
-  const [kategoriaId, setKategoriaId] = useState(0)
+  const [dataAlustettu, setDataAlustettu] = useState(false)
 
+  //const [sukunimi, setSukunimi]=useState("")???
+
+  const initialData = [
+    {
+      uid:uuid(),etunimi: "Pekka", sukunimi: "Jakamo", ikä: 29, jälkikasvu: [{ uid:uuid(),lapsenNimi: "Lissa", nimet: { ensimmäinen_nimi: "Lissa", toinen_nimi: "Riitta" } },
+      { lapsenNimi: "Kaapo" }]
+    },
+    { uid:uuid(),etunimi: "Jarmo", sukunimi: "Jakamo", ikä: 49 }]
+
+  const [selected, setSelected] = useState([])
+  
   useEffect(() => {
-    async function haeDataa() {
-      let result = await axios('https://api.huuto.net/1.1/categories');
-      console.log(JSON.parse(result.request.response).categories);
-      setData(JSON.parse(result.request.response).categories);
 
-    }
 
-    haeDataa();
-
+    
+/* 
+    let jemma = window.localStorage;
+    let tempData = JSON.parse(jemma.getItem("data"))
+    if (tempData == null) {
+      jemma.setItem("data", JSON.stringify(initialData))
+      tempData = initialData
+    } */ 
+    setData(tempData);
+    setDataAlustettu(true)
   }, [])
 
-  const kategoriaNappiPainettu = (id) => {
+  useEffect(() => {
+    if (dataAlustettu) {
+      window.localStorage.setItem("data", JSON.stringify(data))
+    }
+  }, [data])
 
-    setKategoriaValittu(true);
-    setKategoriaId(id);
-    // console.log(id)
+
+  const painikePainettu = () => {
+
+    let uusdata = JSON.parse(JSON.stringify(data));
+    //    let uusdata = [...data];
+
+    //   uusdata[0].jälkikasvu[0].lapsenNimi="Mikko"
+    // let uusdata = [...data];
+    let lopullinenData = data.concat(uusdata)
+    setData(lopullinenData)
+    //setRows([]);
   }
+  const näytäJälkikasvu = (index) => {
+    if (data[index].jälkikasvu !== undefined) {
+      return data[index].jälkikasvu.map((alkio, lapsenIndex) =>
+        <div key={alkio.uid}>
+          <input onChange={(e) => { lapsenNimiMuuttui(e, index, lapsenIndex) }} value={alkio.lapsenNimi}>
+          </input>
+        </div>)
+
+    }
+  }
+  const lapsenNimiMuuttui = (event, vanhemmanIndex, lapsenIndex) => {
+
+    let syväKopio = JSON.parse(JSON.stringify(data))
+    syväKopio[vanhemmanIndex].jälkikasvu[lapsenIndex].lapsenNimi = event.target.value;
+    setData(syväKopio)
+
+  }
+  const sukunimiMuuttui = (event, index) => {
+
+    let syväKopio = JSON.parse(JSON.stringify(data))
+    syväKopio[index].sukunimi = event.target.value;
+    setData(syväKopio)
+
+  }
+  const etunimiMuuttui = (event, index) => {
+
+    let syväKopio = JSON.parse(JSON.stringify(data))
+    syväKopio[index].etunimi = event.target.value;
+    setData(syväKopio)
+
+  }
+
+  const ikäMuuttui = (event, index) => {
+
+    let syväKopio = JSON.parse(JSON.stringify(data))
+    syväKopio[index].ikä = event.target.value;
+    setData(syväKopio)
+
+  }
+  const lisääHenkilö = () => {
+    let syväKopio = JSON.parse(JSON.stringify(data))
+    let uusiHenkilö= {uid:uuid(),etunimi: "", sukunimi: "", ikä: 0}
+    syväKopio.push(uusiHenkilö) 
+    setData(syväKopio)
+  }
+  const poistaHenkilö = (index) => {
+    let syväKopio = JSON.parse(JSON.stringify(data))
+    syväKopio.splice(index,1)
+    setData(syväKopio)
+  }
+
+  // opettajan koodi commit
 
   return (<div>
 
-    {data.map(alkio => <button onClick={() => kategoriaNappiPainettu(alkio.id)}>{alkio.title}</button>)
-    }
-    {kategoriaValittu && <Esineet id={kategoriaId} ></Esineet>}
-  </div>)
+    {data.map((item, index) => <div key={item.uid}>
+    <input onChange={(event) => etunimiMuuttui(event, index)}
+        value={item.etunimi}> 
+      </input>
+      <input onChange={(event) => sukunimiMuuttui(event, index)}
+        value={item.sukunimi}>
+      </input>
+      <input onChange={(event) => ikäMuuttui(event, index)}
+         value={item.ikä}>
+      </input>
+      <button onClick={()=>poistaHenkilö(index)}>Poista henkilö</button>
+      {item.jälkikasvu ? <LapsiLista lapsenNimiMuuttui={lapsenNimiMuuttui} parentIndex={index} lapsiLista={item.jälkikasvu}></LapsiLista> : ""}
+    </div>)}
+
+
+    <button onClick={lisääHenkilö}>Lisää henkilö</button>
+  </div>
+  );
 }
 
-export default App
+export default App;
