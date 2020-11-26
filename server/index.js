deff40ed32a85d94ec085492426177c39bf0ea7e
laineantti <laineantti@users.noreply.github.com>
2020-11-26 21:49:35 +0200

    testailtu express ja cors

--- server/index.js
+++ server/index.js
@@ -1,32 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 4000
-
-// notice here I'm requiring my database adapter file
-// and not requiring node-postgres directly
-const db = require('./db')
-app.get('/:id', (req, res, next) => {
-  db.query('SELECT * FROM Tentti WHERE id = $1', [req.params.id], (err, result) => {
-    if (err) {
-      return next(err)
-    }
-    result.send(result.rows[0])
-  })
+var express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+ 
+var app = express()
+module.exports = app
+var port = process.env.PORT || 5000
+app.use(bodyParser.json())
+//https://expressjs.com/en/resources/middleware/cors.html
+app.use(cors())
+app.post('/poistatentti', (req, res) => {
+  
+  console.log(req.body)
+  res.send('Tähän tulee sitten dataa tietokannasta tai jotain muuta')
 })
-// ... many other routes in this file
-
-/* app.get('/', (req, res) => {
-  res.send('Hello World GET!')
-}) */
-
 app.post('/', (req, res) => {
-  res.send('Hello World POST!')
+  
+  console.log(req.body)
+  res.send('Tähän tulee sitten dataa tietokannasta tai jotain muuta')
 })
-
-app.delete('/', (req, res) => {
-  res.send('Hello World DELETE!')
+app.post('/lisäätentti', (req, res) => {
+  
+  console.log(req.body)
+  res.send('Tähän tulee sitten dataa tietokannasta tai jotain muuta')
 })
-
+app.post('/haekaikkitentit', (req, res) => {
+  
+  console.log(req.body)
+  res.send('Tähän tulee sitten dataa tietokannasta tai jotain muuta')
+})
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log("Palvelin käynnistyi portissa: " + port)
 })
